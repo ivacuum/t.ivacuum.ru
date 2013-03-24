0fdd42d2c8bc94220073e48d9b86c685f7487870
Sergey Pankov <vacuum@ivacuum.ru>
2013-03-24 10:37:08 +0400

    Переход на стабильную версию фреймворка 1.2.2

--- cron.php
+++ cron.php
@@ -11,7 +11,7 @@ namespace app;
 $_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/public_html';
 $_SERVER['SERVER_NAME'] = basename(__DIR__);
 
-require('../_/fw/1.2.0/bootstrap.php');
+require('../_/fw/1.2.2/bootstrap.php');
 
 /* Выполнение задач */
 $app['cron']->run();
