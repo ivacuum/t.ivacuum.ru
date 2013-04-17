f1c20cb319b677af945131f47b9be1cb7223cd97
Sergey Pankov <vacuum@ivacuum.ru>
2013-04-17 17:31:21 +0400

    Использование фреймворка для формирования и выдачи поисковых подсказок

--- public_html/suggest.php
+++ public_html/suggest.php
@@ -8,16 +8,15 @@ require 'common.php';
 
 $user->session_start();
 
-$q         = $app['request']->variable('term', '');
+$q = $app['request']->variable('term', '');
 $q_max_len = 60;
 
-if( $q == 'поиск...' || !$q || mb_strlen($q) < 2 )
+if ($q == 'поиск...' || !$q || mb_strlen($q) < 2)
 {
 	bb_exit();
-	// exit();
 }
 
-if( $tmp = mb_substr($q, 0, $q_max_len) )
+if ($tmp = mb_substr($q, 0, $q_max_len))
 {
 	$tmp = preg_replace('#(?<=\S)\-#', ' ', $tmp);                    // "1-2-3" -> "1 2 3"
 	$tmp = preg_replace('#[^\da-zA-Z\x{7f}-\x{ff}\-_*|]#', ' ', $tmp);    // допустимые символы (кроме " которые отдельно)
@@ -31,26 +30,24 @@ if( $tmp = mb_substr($q, 0, $q_max_len) )
 	$q = str_compact($tmp);
 }
 
-if( !$q || mb_strlen($q) < 2 )
+if (!$q || mb_strlen($q) < 2)
 {
 	bb_exit();
-	// exit();
 }
 
-$ary = array();
-$ary[] = array(
+$ary[] = [
 	'icon'  => $static_path . '/i/_/magnifier.png',
 	'label' => $q,
-);
+];
 
 $excluded_forums_csv = $user->get_excluded_forums(AUTH_READ);
 
-require SITE_DIR . 'includes/db/sphinx.php';
+// require SITE_DIR . 'includes/db/sphinx.php';
 
-$sphinx = new db_sphinx();
-$sphinx->connect($bb_cfg['sphinx']['host'], $bb_cfg['sphinx']['port'], $bb_cfg['sphinx']['socket']);
+// $sphinx = new db_sphinx();
+// $sphinx->connect($bb_cfg['sphinx']['host'], $bb_cfg['sphinx']['port'], $bb_cfg['sphinx']['socket']);
 
-if( false === strpos($q, ' ') && false === strpos($q, '*') )
+if (false === strpos($q, ' ') && false === strpos($q, '*'))
 {
 	$query = $q . '*';
 }
@@ -73,29 +70,35 @@ if( $excluded_forums_csv )
 $sql .= ' ORDER BY seeders DESC';
 $sql .= ' LIMIT 0, 10';
 $sql .= ' OPTION ranker = none';
-$result = $sphinx->query($sql);
+$app['sphinx']->query($sql);
 
-$tor_list_ary = array();
+$tor_list_ary = [];
 
-while( $row = $sphinx->fetchrow($result) )
+while ($row = $app['sphinx']->fetchrow())
 {
 	$tor_list_ary[] = $row['id'];
 }
 
-$sphinx->freeresult($result);
-$sphinx->close();
+$app['sphinx']->freeresult();
 
-if( !sizeof($tor_list_ary) )
+if (!sizeof($tor_list_ary))
 {
 	bb_exit(json_encode($ary));
-	// exit(json_encode($ary));
 }
 
 $tor_list_sql = join(',', $tor_list_ary);
 $tor_count = sizeof($tor_list_ary);
 
-$sql = 'INSERT INTO bb_search_queries (user_id, search_query, search_time, search_results, search_suggest) VALUES (' . $userdata['user_id'] . ', "' . $db->escape($q) . '", ' . time() . ', ' . $tor_count . ', 1)';
-$db->sql_query($sql);
+$sql_ary = [
+	'user_id'        => $userdata['user_id'],
+	'search_query'   => $q,
+	'search_time'    => time(),
+	'search_results' => $tor_count,
+	'search_suggest' => 1,
+];
+
+$sql = 'INSERT INTO bb_search_queries ' . $app['db']->build_array('INSERT', $sql_ary);
+$app['db']->query($sql);
 
 $sql = '
 	SELECT
@@ -109,20 +112,18 @@ $sql = '
 	LEFT JOIN
 		bb_forums f ON (f.forum_id = tor.forum_id)
 	WHERE
-		tor.topic_id IN(' . $tor_list_sql . ')';
-$result = $db->sql_query($sql);
+		tor.topic_id IN (' . $tor_list_sql . ')';
+$app['db']->query($sql);
 
-while( $row = $db->sql_fetchrow($result) )
+while ($row = $app['db']->fetchrow())
 {
-	$ary[] = array(
+	$ary[] = [
 		'icon'  => $static_path . '/i/_/' . ($row['forum_icon'] ?: 'question_balloon') . '.png',
 		'label' => $row['topic_title'],
 		'link'  => '/viewtopic.php?t=' . $row['topic_id'],
-	);
+	];
 }
 
-$db->sql_freeresult($result);
+$app['db']->freeresult();
 
-print json_encode($ary);
-
-bb_exit();
+bb_exit(json_encode($ary));
