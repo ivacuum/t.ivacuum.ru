a16ebbf5322c01dc55126e4cbedd1ac48991349a
Sergey Pankov <vacuum@ivacuum.ru>
2013-04-17 17:05:05 +0400

    Использование фреймворка при построении топа скоростей

--- public_html/top_speed.php
+++ public_html/top_speed.php
@@ -10,9 +10,9 @@ require 'common.php';
 // Session start
 $user->session_start(array('req_login' => $bb_cfg['bt_tor_browse_only_reg']));
 
-$sort = request_var('sort', '');
+$sort = $app['request']->variable('sort', '');
 
-$sort_sql = ( $sort == 'download' ) ? 'speed_down DESC, speed_up DESC' : 'speed_up DESC, speed_down DESC';
+$sort_sql = $sort == 'download' ? 'speed_down DESC, speed_up DESC' : 'speed_up DESC, speed_down DESC';
 
 $sql = '
 	SELECT
@@ -22,59 +22,56 @@ $sql = '
 		u.username,
 		u.user_level
 	FROM
-		bb_bt_tracker t,
-		bb_users u
+		bb_bt_tracker t
+	LEFT JOIN
+		bb_users u ON (u.user_id = t.user_id)
 	WHERE
-		t.user_id = u.user_id
-	AND
 		(speed_up != 0 OR speed_down != 0)
 	GROUP BY
 		t.user_id
 	ORDER BY
-		' . $sort_sql . '
-	LIMIT
-		0, 50';
-$result = $db->sql_query($sql);
+		:order_by';
+$app['db']->query_limit($sql, [':order_by' => $sort_sql], 50);
 
 $i = 1;
 
-while( $row = $db->sql_fetchrow($result) )
+while ($row = $app['db']->fetchrow())
 {
 	$username = $row['username'];
 
-	if( $row['user_level'] == ADMIN )
+	if ($row['user_level'] == ADMIN)
 	{
 		$username = '<b class="colorAdmin">' . $username . '</b>';
 	}
-	elseif( $row['user_level'] == MOD )
+	elseif ($row['user_level'] == MOD)
 	{
 		$username = '<b class="colorMod">' . $username . '</b>';
 	}
-	elseif( $row['user_level'] == GROUP_MEMBER )
+	elseif ($row['user_level'] == GROUP_MEMBER)
 	{
 		$username = '<b class="colorGroup">' . $username . '</b>';
 	}
 
-	$template->assign_block_vars('memberrow', array(
-		'DOWNLOAD'   => ( $row['speed_down'] ) ? humn_size($row['speed_down']) . '/с' : '&mdash;',
+	$template->assign_block_vars('memberrow', [
+		'DOWNLOAD'   => $row['speed_down'] ? humn_size($row['speed_down']) . '/с' : '&mdash;',
 		'ROW_NUMBER' => $i,
 		'ROW_CLASS'  => !($i % 2) ? 'row2' : 'row1',
-		'UPLOAD'     => ( $row['speed_up'] ) ? humn_size($row['speed_up']) . '/с' : '&mdash;',
+		'UPLOAD'     => $row['speed_up'] ? humn_size($row['speed_up']) . '/с' : '&mdash;',
 		'USERNAME'   => $username,
 
-		'U_VIEWPROFILE' => append_sid("profile.php?mode=viewprofile&amp;". POST_USERS_URL ."=" . $row['user_id']))
-	);
+		'U_VIEWPROFILE' => append_sid("profile.php?mode=viewprofile&amp;". POST_USERS_URL ."=" . $row['user_id']),
+	]);
 
 	$i++;
 }
 
-$db->sql_freeresult($result);
+$app['db']->freeresult();
 
-$template->assign_vars(array(
-	'SORT_BY' => ( $sort == 'download' ) ? 'dl' : 'up',
+$template->assign_vars([
+	'SORT_BY' => $sort == 'download' ? 'dl' : 'up',
 
 	'U_SORT_BY_DOWNLOAD' => append_sid('top_speed.php?sort=download'),
-	'U_SORT_BY_UPLOAD'   => append_sid('top_speed.php?sort=upload'))
-);
+	'U_SORT_BY_UPLOAD'   => append_sid('top_speed.php?sort=upload'),
+]);
 
 print_page('top_speed.tpl');
