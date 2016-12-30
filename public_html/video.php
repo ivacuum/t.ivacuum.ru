7bd0b31e13885b2ea3bac91c49bc232cc61263af
Sergey Pankov <vacuum@ivacuum.ru>
2016-12-30 14:12:03 +0300

    Видос как сидировать раздачи

---
+++ public_html/video.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-style-type" content="text/css" />
+<link rel="shortcut icon" href="//0.ivacuum.org/i/t/images/siteicon.png" />
+<link rel="stylesheet" href="//0.ivacuum.org/i/t/css/style.css?v=19" type="text/css" />
+<script type="text/javascript">
+var S_LOCAL = true;
+</script>
+<script type="text/javascript" src="//0.ivacuum.org/js/jquery/1.8.2/jquery.pack.js?v=13"></script>
+<link rel="stylesheet" href="//0.ivacuum.org/js/flowplayer/5.3.2/skin/functional.css">
+<script src="//0.ivacuum.org/js/flowplayer/5.3.2/flowplayer.min.js?v=13"></script>
+<style>
+.flowplayer { background-color: #222; }
+</style>
+<body>
+<div data-ratio="0.5625" data-embed="false" data-volume="0.5" class="flowplayer">
+	<video preload="none">
+		<source type="video/mp4" src="//0.ivacuum.org/v/seeding_howto.mp4">
+		<source type="video/webm" src="//0.ivacuum.org/v/seeding_howto.webm">
+	</video>
+</div>
+</body>
+</html>
